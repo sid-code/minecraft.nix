afd94b320f882e3f7d5e4e05c450010419f195df
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2024-01-21 14:51:34 -0800

    add a dev shell for messing around in python repl
    also format with alejandra

--- flake.nix
+++ flake.nix
@@ -9,25 +9,33 @@
     inputs.flake-utils.follows = "flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, metadata }:
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    metadata,
+  }:
     with flake-utils.lib;
-    eachDefaultSystem (system:
-      let
+      eachDefaultSystem (system: let
         inherit (pkgs) lib;
         pkgs = nixpkgs.legacyPackages.${system};
         OS = with pkgs.stdenv;
-          if isLinux then
-            "linux"
-          else if isDarwin then
-            "osx"
-          else
-            builtins.throw "Unsupported system ${system}";
-        py = pkgs.python3.withPackages (p: [ p.requests ]);
+          if isLinux
+          then "linux"
+          else if isDarwin
+          then "osx"
+          else builtins.throw "Unsupported system ${system}";
+        py = pkgs.python3.withPackages (p: [p.requests]);
       in {
         legacyPackages = lib.makeOverridable (import ./all-packages.nix) {
           inherit pkgs lib metadata OS;
           # Users may override this with their own application id
           authClientID = "7e6f5a7f-17fb-420e-81f2-96ee1a11615b";
+        };
+        devShells.auth = pkgs.mkShell {
+          nativeBuildInputs = [
+            (pkgs.python3.withPackages (p: (import ./module/auth-libs.nix {python3Packages = p;})))
+          ];
         };
         apps.update = mkApp {
           drv = let
@@ -36,10 +44,11 @@
               ${py}/bin/python update.py
               popd
             '';
-          in pkgs.writeShellScriptBin "update" ''
-            ${snippet "fabric"}
-            ${snippet "vanilla"}
-          '';
+          in
+            pkgs.writeShellScriptBin "update" ''
+              ${snippet "fabric"}
+              ${snippet "vanilla"}
+            '';
         };
       });
 }
