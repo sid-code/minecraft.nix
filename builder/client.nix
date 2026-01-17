32b3b7d41d23dbcb877137a4067758337396b551
Sid Kulkarni <kulkarnisidharth1@gmail.com>
2026-01-17 09:24:38 -0800

    use libglvnd instead of libgl
    apparently you're not supposed to use libglvnd but on aarch64-darwin
    pkgs.libGL is null (!)

--- builder/client.nix
+++ builder/client.nix
@@ -1,7 +1,19 @@
-{ pkgs, lib, authClientID, OS }:
+{
+  pkgs,
+  lib,
+  authClientID,
+  OS,
+}:
 with lib;
 let
-  inherit (pkgs) fetchurl libpulseaudio libGL flite alsa-lib unzip runCommand;
+  inherit (pkgs)
+    libpulseaudio
+    libglvnd
+    flite
+    alsa-lib
+    unzip
+    runCommand
+    ;
   inherit (pkgs.xorg) libXcursor libXrandr libXxf86vm;
 
   preloadLibraries = [
@@ -9,63 +21,78 @@ let
     libXcursor
     libXrandr
     libXxf86vm # Needed only for versions <1.13
-    libGL
+    libglvnd # To fix issue on macos where pkgs.libGL is null
     flite
     alsa-lib
   ];
 
-  buildVanillaLibraries = artifacts:
-    map (lib: fetchurl { inherit (lib.downloads.artifact) url sha1; })
-    (filter (x: !(x.downloads ? "classifiers")) artifacts);
-  buildNativeLibraries = artifacts:
-    map (artif:
+  buildVanillaLibraries =
+    artifacts:
+    map (lib: fetchurl { inherit (lib.downloads.artifact) url sha1; }) (
+      filter (x: !(x.downloads ? "classifiers")) artifacts
+    );
+  buildNativeLibraries =
+    artifacts:
+    map (
+      artif:
       let
         zip = fetchurl {
           inherit (artif.downloads.classifiers.${artif.natives.${OS}}) url sha1;
         };
-      in runCommand "native" { buildInputs = [ unzip ]; } ''
+      in
+      runCommand "native" { buildInputs = [ unzip ]; } ''
         mkdir -p $out/lib
         unzip ${zip} -d $out/lib
         rm -rf $out/lib/META-INF
-      '') (filter (x: (x.downloads ? "classifiers")) artifacts);
-  buildAssets = versionInfo: assetsIndex:
+      ''
+    ) (filter (x: (x.downloads ? "classifiers")) artifacts);
+  buildAssets =
+    versionInfo: assetsIndex:
     runCommand "assets" { } ''
-      ${concatStringsSep "\n" (builtins.attrValues
-        (flip mapAttrs assetsIndex.objects (name: a:
-          let
-            asset = fetchurl {
-              sha1 = a.hash;
-              url = "https://resources.download.minecraft.net/" + hashTwo;
-            };
-            hashTwo = builtins.substring 0 2 a.hash + "/" + a.hash;
-          in ''
-            mkdir -p $out/objects/${builtins.substring 0 2 a.hash}
-            ln -sf ${asset} $out/objects/${hashTwo}
-          '')))}
+      ${concatStringsSep "\n" (
+        builtins.attrValues (
+          flip mapAttrs assetsIndex.objects (
+            name: a:
+            let
+              asset = fetchurl {
+                sha1 = a.hash;
+                url = "https://resources.download.minecraft.net/" + hashTwo;
+              };
+              hashTwo = builtins.substring 0 2 a.hash + "/" + a.hash;
+            in
+            ''
+              mkdir -p $out/objects/${builtins.substring 0 2 a.hash}
+              ln -sf ${asset} $out/objects/${hashTwo}
+            ''
+          )
+        )
+      )}
       mkdir -p $out/indexes
       ln -s ${builtins.toFile "assets.json" (builtins.toJSON assetsIndex)} \
           $out/indexes/${versionInfo.assets}.json
     '';
   buildFabricLibraries = libraries: map (lib: fetchJar lib) libraries;
 
-  buildBasicModule = versionInfo: assetsIndex:
+  buildBasicModule =
+    versionInfo: assetsIndex:
     let
       client = fetchurl { inherit (versionInfo.downloads.client) url sha1; };
-      isAllowed = artifact:
+      isAllowed =
+        artifact:
         let
-          lemma1 = acc: rule:
+          lemma1 =
+            acc: rule:
             if rule.action == "allow" then
               if rule ? os then rule.os.name == OS else true
             else if rule ? os then
               rule.os.name != OS
             else
               false;
-        in if artifact ? rules then
-          foldl' lemma1 false artifact.rules
-        else
-          true;
+        in
+        if artifact ? rules then foldl' lemma1 false artifact.rules else true;
       artifacts = lib.filter isAllowed versionInfo.libraries;
-    in {
+    in
+    {
       inherit authClientID;
       version = versionInfo.id;
       java = mkDefault (defaultJavaVersion versionInfo);
@@ -81,13 +108,14 @@ let
     { inherit (versionInfo) mainClass; }
   ];
 
-  buildFabricModules = versionInfo: assetsIndex: fabricProfile:
+  buildFabricModules =
+    versionInfo: assetsIndex: fabricProfile:
     let
       loaderVersion = fabricProfile.loader;
       loader = fabricLoaders.${loaderVersion};
-      extraJavaLibraries = buildFabricLibraries
-        (fabricProfile.libraries.client ++ loader.libraries);
-    in [
+      extraJavaLibraries = buildFabricLibraries (fabricProfile.libraries.client ++ loader.libraries);
+    in
+    [
       (buildBasicModule versionInfo assetsIndex)
       ({
         libraries.java = extraJavaLibraries;
@@ -95,7 +123,8 @@ let
       })
     ];
 
-in {
+in
+{
   build = mkBuild {
     baseModulePath = ../module/client.nix;
     inherit buildFabricModules buildVanillaModules;
--- flake.nix
+++ flake.nix
@@ -9,46 +9,58 @@
     inputs.flake-utils.follows = "flake-utils";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-    metadata,
-  }:
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      metadata,
+    }:
     with flake-utils.lib;
-      eachDefaultSystem (system: let
+    eachDefaultSystem (
+      system:
+      let
         inherit (pkgs) lib;
         pkgs = nixpkgs.legacyPackages.${system};
-        OS = with pkgs.stdenv;
-          if isLinux
-          then "linux"
-          else if isDarwin
-          then "osx"
-          else builtins.throw "Unsupported system ${system}";
-        py = pkgs.python3.withPackages (p: [p.requests]);
-      in {
+        OS =
+          with pkgs.stdenv;
+          if isLinux then
+            "linux"
+          else if isDarwin then
+            "osx"
+          else
+            throw "Unsupported system ${system}";
+        py = pkgs.python3.withPackages (p: [ p.requests ]);
+      in
+      {
         legacyPackages = lib.makeOverridable (import ./all-packages.nix) {
-          inherit pkgs lib metadata OS;
+          inherit
+            pkgs
+            lib
+            metadata
+            OS
+            ;
           # Users may override this with their own application id
           authClientID = "7e6f5a7f-17fb-420e-81f2-96ee1a11615b";
         };
         devShells.auth = pkgs.mkShell {
           nativeBuildInputs = [
-            (pkgs.python3.withPackages (p: (import ./module/auth-libs.nix {python3Packages = p;})))
+            (pkgs.python3.withPackages (p: (import ./module/auth-libs.nix { python3Packages = p; })))
           ];
         };
         apps.update = mkApp {
-          drv = let
-            snippet = dir: ''
-              pushd ./${dir}
-              ${py}/bin/python update.py
-              popd
-            '';
-          in
+          drv =
+            let
+              snippet = dir: ''
+                pushd ./${dir}
+                ${py}/bin/python update.py
+                popd
+              '';
+            in
             pkgs.writeShellScriptBin "update" ''
               ${snippet "fabric"}
               ${snippet "vanilla"}
             '';
         };
-      });
+      }
+    );
 }
